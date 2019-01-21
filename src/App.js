7c70d87864b68b119fa41df8092d9f8c22fd97f6
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-21 12:00:10 +0100

    revised app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>Welcome to Codaisseur!</h2>
+          <h2>Welcome to Codaisseur</h2>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -21,6 +22,10 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <main>
+          <Title />
+        </main>
       </div>
     );
   }
