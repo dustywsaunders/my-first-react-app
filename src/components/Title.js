b7023fba767c65a9f709de8cac2fbd9971b702b0
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-21 11:47:21 +0100

    title component added

---
+++ src/components/Title.js
@@ -0,0 +1,39 @@
+
+import React, { Component } from 'react'
+
+export default class Title extends Component {
+  render() {
+    return 'test'
+  }
+}
+
+
+
+
+// // ADVICE FROM KELLEY
+// // defined by react in their code
+
+// class component {
+//     add_to_dom () {
+//         let el = this.render();
+//         document.addElemen(el);
+//     }
+
+//     // it excplicitly does not have dream
+//     // you have to define it yourself
+// }
+
+// // define yourself
+
+// let Mammal = require("react").Mammal;
+
+// class button extends Mammal {
+//     render () {
+//         // 
+//     }
+// }
+
+// <person></person>
+
+// dusty = new Person();
+// dusty.sleep();
